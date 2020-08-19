2066e244eade8db2665fd6ab7c987626b6515b20
ronak-soni <ronakvsoni@gmail.com>
2020-08-19 15:15:36 -0700

    ronak local changes

--- app/controllers/admins_controller.rb
+++ app/controllers/admins_controller.rb
@@ -11,7 +11,7 @@ class AdminsController < ApplicationController
             redirect_to @admin
         else
             flash[:message] = @admin.errors.full_messages
-            render :new
+            render 'new'
         end
     end
 
@@ -20,13 +20,18 @@ class AdminsController < ApplicationController
     end
 
     def show
-    
+        @Admin = Admin.find(params[:id])
+    end
+
+
+    def index
+        
     end
 
 private
 
     def admin_params
-        params.require(:admin).permit(:name, :email, :position)
+        params.require(:admin).permit(:name,:username, :email, :position, :password, :password_confirmation)
     end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
   resources :team_members
   resources :admins
  
-  
+  post '/admins', to: 'admins#create'
   root to: "auth#index"
   # get '/login', to: "admin#new", as: "login"
   # get 'new_admin_path', to: 'admins#new', as: 'signup'
