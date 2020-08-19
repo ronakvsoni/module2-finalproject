ebd28e92150f7d8f8e6a418bfffbd907c07470f4
ronak-soni <ronakvsoni@gmail.com>
2020-08-18 17:32:15 -0700

    working in password authetication

--- app/controllers/admins_controller.rb
+++ app/controllers/admins_controller.rb
@@ -1,2 +1,32 @@
 class AdminsController < ApplicationController
+
+    def new
+        @admin=Admin.new  
+    end
+
+    def create
+        @admin = Admin.new(admin_params)
+        if @admin.save
+            session[:user_id] = @admin.id
+            redirect_to @admin
+        else
+            flash[:message] = @admin.errors.full_messages
+            render :new
+        end
+    end
+
+    def edit
+        @Admin = Admin.find(params[:id])
+    end
+
+    def show
+    
+    end
+
+private
+
+    def admin_params
+        params.require(:admin).permit(:name, :email, :position)
+    end
+
 end
